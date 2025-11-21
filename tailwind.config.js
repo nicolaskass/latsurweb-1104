b598ec7d04e31c1d2e1e0586331e963d599679ea
nicolaskass <nkass@fcnym.unlp.edu.ar>
2025-11-21 13:14:06 -0300

    fixeando que no andaba

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: [
     "./index.html",
-    "./sections/**/*.html",
+    "./sections/contacto.html",
+    "./sections/hero.html",
+    "./sections/nosotros.html",
+    "./sections/servicios.html",
+    "./sections/trabajos.html"
   ],
   theme: {
     extend: {},
